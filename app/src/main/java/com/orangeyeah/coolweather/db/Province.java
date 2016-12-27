dd413c166eff74f7e94262b8e01f8f1ebc13f907
orangeyeah <391110787@qq.com>
2016-12-27 17:14:11 +0800

    Second commit.Add db and dependencies.

---
+++ app/src/main/java/com/orangeyeah/coolweather/db/Province.java
@@ -0,0 +1,40 @@
+package com.orangeyeah.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * 创建日期:2016/12/27/0027
+ * 公司:OrangeYeah
+ * 作者:李楠
+ */
+
+public class Province extends DataSupport{
+
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
